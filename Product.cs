6d8f01e5288a96608d7094a49126ce9d9b8e00a8
fulltimejob93 <pupokivanh@gmail.com>
2026-03-30 17:56:23 +0300

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.Model;
+
+public class AnimeCharacter
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string AnimeTitle { get; set; }
+    public required string Role { get; set; } // Например: Протагонист, Антагонист
+    public int PowerLevel { get; set; }
+
+    public override string ToString()
+    {
+        return $"Id: {Id}, Name: {Name}, Anime: {AnimeTitle}, Role: {Role}, Power: {PowerLevel}";
+    }
+}
